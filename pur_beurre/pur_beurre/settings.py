9acf8f00e384d17c74d8a17d5869ded6334937b4
Ferrandeau Mathieu <m.ferrandeau@hotmail.fr>
2019-10-24 18:16:27 +0200

    modifications

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -135,10 +135,14 @@ STATIC_URL = '/static/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
 
+    # Static files settings
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static')
+        os.path.join(PROJECT_ROOT, 'static'),
     )
 
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- pur_beurre/pur_beurre/urls.py
+++ pur_beurre/pur_beurre/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views 
+from users import views
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('content/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
     path('favorite/', views.favorite, name='favorite'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
